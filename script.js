0be37d658d23a66c7e8e732fac3a139247d7c5ec
oksana-shpak <oksana.shpak@convertdigital.com.au>
2025-08-24 16:24:27 +1000

    Refactor flower drawing logic to use Shape class and improve color calculation

--- script.js
+++ script.js
@@ -4,39 +4,68 @@ document.body.appendChild(canvas);
 
 canvas.width = window.innerWidth * 4;
 canvas.height = window.innerHeight * 4;
+class Shape {
+  constructor(color, ...points) {
+    this.points = points.map(([x, y]) => [x * unit, y * unit]);
+    this.color = color;
+  }
+}
 
 const cx = canvas.width / 2;
 const cy = canvas.height / 2;
 
 const unit = Math.min(cx, cy) / 50;
-const color = { hue: [0, 360], saturation: [100, 100], lightness: [60, 60] };
+const color1 = { hue: [0, 360], saturation: [100, 100], lightness: [60, 60] };
+const color2 = { hue: [30, 60], saturation: [100, 100], lightness: [60, 60] };
 
-const triangle = [[0, 0], [0, - 25 * unit], [5 * unit, - 15 * unit], color];
-const petalCount = 36;
+const shape1 = new Shape(color1, [0, 0], [-5, -15], [0, -25], [5, -15]);
+const shape2 = new Shape(color2, [20, 20], [15, 5], [25, 15]);
+
+const petalCount = 24;
 
 ctx.translate(cx, cy);
 
-drawFlower(petalCount, triangle)
+drawFlower(petalCount, shape1, shape2);
 
-function drawTriangle(p1, p2, p3, color) {
+function drawPolygon(points, color) {
   ctx.fillStyle = color;
   ctx.beginPath();
-  ctx.moveTo(...p1);
-  ctx.lineTo(...p2);
-  ctx.lineTo(...p3);
+  ctx.moveTo(...points[0]);
+  for (let i = 1; i < points.length; i++) {
+    ctx.lineTo(...points[i]);
+  }
   ctx.closePath();
   ctx.fill();
 }
 
-function drawFlower(petalCount, shape) {
-  const [p1, p2, p3, color] = shape;
-  const { hue, saturation, lightness } = color;
-  for (let i = 0; i < petalCount; i++) {
-    const angle = i * 2 * Math.PI;
-    ctx.save();
-    ctx.rotate(angle / petalCount);
-    const color = `hsl(${hue[0] + (hue[1] - hue[0]) * i / petalCount}, ${saturation[0] + (saturation[1] - saturation[0]) * i / petalCount}%, ${lightness[0] + (lightness[1] - lightness[0]) * i / petalCount}%)`;
-    drawTriangle(p1, p2, p3, color);
-    ctx.restore();
+function drawFlower(petalCount, ...petalShapes) {
+  for (const petal of petalShapes) {
+    const { points, color } = petal;
+    const { hue, saturation, lightness } = color;
+    for (let i = 0; i < petalCount; i++) {
+      const angle = i * 2 * Math.PI;
+      const color = calcColor(i, petalCount, hue, saturation, lightness);
+      ctx.save();
+      ctx.rotate(angle / petalCount);
+      drawPolygon(points, color);
+      ctx.restore();
+    }
   }
+}
+
+function calcColor(i, petalCount, hue, saturation, lightness) {
+  const firstHalf = i < petalCount / 2;
+  const h = firstHalf
+    ? hue[0] + (hue[1] - hue[0]) * i / petalCount * 2
+    : hue[1] - (hue[1] - hue[0]) * i / petalCount * 2;
+  petalCount /= 2;
+  const s = firstHalf
+    ? saturation[0] + (saturation[1] - saturation[0]) * i / petalCount
+    : saturation[1] - (saturation[1] - saturation[0]) * i / petalCount;
+  const l = firstHalf
+    ? lightness[0] + (lightness[1] - lightness[0]) * i / petalCount
+    : lightness[1] - (lightness[1] - lightness[0]) * i / petalCount;
+
+  const color = `hsl(${h}, ${s}%, ${l}%)`;
+  return color;
 }
